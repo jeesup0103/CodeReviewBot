cc7eeee3d906bfe1f24cdd81f5636d64dc5fe83f
jeesup0103 <jeesup0103@gmail.com>
2025-02-26 23:01:47 +0900

    Change bot profile

--- review_code.py
+++ review_code.py
@@ -5,16 +5,17 @@ from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
+
 # Set your OpenAI API key
 client = OpenAI()
 
 def get_pr_details(pr_url):
     headers = {
-        'Authorization': f'token {os.getenv("GITHUB_TOKEN")}',
-        'Accept': 'application/vnd.github.dif'
+        'Authorization': f'token {os.getenv("REPO_GH_TOKEN")}',  # Using repository token for reading PR details
+        'Accept': 'application/vnd.github.v3+json'
     }
     response = requests.get(pr_url, headers=headers)
-    
+
     if response.status_code == 200:
         return response.json()  # Parse and return the JSON content as a dictionary
     else:
@@ -22,7 +23,7 @@ def get_pr_details(pr_url):
 
 def get_code_changes(base_url, base_sha, head_sha):
     headers = {
-        'Authorization': f'token {os.getenv("GITHUB_TOKEN")}',
+        'Authorization': f'token {os.getenv("REPO_GH_TOKEN")}',  # Using repository token
         'Accept': 'application/vnd.github.v3.diff'
     }
     response = requests.get(f"{base_url}/compare/{base_sha}...{head_sha}", headers=headers)
@@ -31,20 +32,20 @@ def get_code_changes(base_url, base_sha, head_sha):
     else:
         raise Exception(f"Failed to fetch code changes: {response.status_code} {response.text}")
 
-def analyze_code_with_chatgpt(code_diff):
+def analyze_code(code_diff):
     # Read system prompt from review_prompt.txt file
-    with open('llm_code_review/review_prompt.txt', 'r') as file:
+    with open('./review_prompt.txt', 'r') as file:
         system_prompt = file.read()
-        
+
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo",  # The appropriate model you are subscribed to
+        model="gpt-3.5-turbo",
         messages=[
             {
-                "role": "system", 
+                "role": "system",
                 "content": system_prompt
             },
             {
-                "role": "user", 
+                "role": "user",
                 "content": f"{code_diff}"
             },
         ],
@@ -62,7 +63,7 @@ def post_feedback_to_github(pr_url, feedback):
     response = requests.post(pr_comments_url, json={
         "body": feedback
     }, headers={
-        'Authorization': f'token {os.getenv("GITHUB_TOKEN")}',
+        'Authorization': f'token {os.getenv("BOT_GH_TOKEN")}',
         'Content-Type': 'application/json'
     })
 
@@ -82,7 +83,7 @@ def main(pr_url):
 
     # Get the code changes between the base and head commit
     code_diff = get_code_changes(repo_url, base_sha, head_sha)
-    feedback = analyze_code_with_chatgpt(code_diff)
+    feedback = analyze_code(code_diff)
     post_feedback_to_github(pr_url, feedback)
 
 if __name__ == "__main__":
